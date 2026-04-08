5795a2e60c77b0c791b7827620cb5ec233e9f59e
Shaik Siddique <shaik.siddique.ext@orange.com>
2026-04-08 12:28:17 +0530

    feat(hoildays): add holidays module

---
+++ src/main/java/com/ionidea/mothramxbe/repository/RefMonthRepository.java
@@ -0,0 +1,7 @@
+package com.ionidea.mothramxbe.repository;
+
+import com.ionidea.mothramxbe.entity.RefMonth;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefMonthRepository extends JpaRepository<RefMonth, Long> {
+}
