785297d7c44ab409b1e7fbd6b61f876ff767acc5
Shaik Siddique <shaik.siddique.ext@orange.com>
2026-04-08 11:35:33 +0530

    first commit

---
+++ src/main/java/com/ionidea/mothramxbe/MothramxbeApplication.java
@@ -0,0 +1,13 @@
+package com.ionidea.mothramxbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MothramxbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MothramxbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ionidea/mothramxbe/MothramxbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ionidea.mothramxbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MothramxbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
